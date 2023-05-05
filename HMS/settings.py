26bf776429fab88ddcbaaad79777ad20f7ba8913
anupam.laddha <anupam.laddha@e-stonetech.com>
2023-05-05 12:26:25 +0530

    Changes in settings

--- HMS/settings.py
+++ HMS/settings.py
@@ -19,7 +19,7 @@ env = environ.Env(
 DEBUG=(bool, False)
 )
 
-environ.Env.read_env()
+# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+# SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
