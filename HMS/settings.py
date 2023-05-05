02beb4655b3635ca8a67f1e662e9ff1818f5c8db
Anupamladdha <56990426+Anupamladdha@users.noreply.github.com>
2023-05-05 14:47:43 +0530

    Update settings.py

--- HMS/settings.py
+++ HMS/settings.py
@@ -29,12 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = "dsdsdsadas"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
